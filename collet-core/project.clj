1cf42ea860e5951f493365dd299f7bb315707780
Sergey <sergey@minorodata.com>
2025-01-15 18:04:01 +0100

    Version 0.1.2-SNAPSHOT

--- collet-core/project.clj
+++ collet-core/project.clj
@@ -1,4 +1,4 @@
-(defproject io.velio/collet-core "0.1.1"
+(defproject io.velio/collet-core "0.1.2-SNAPSHOT"
   :description "Collet core library"
   :url "https://github.com/velio-io/collet"
   :license
