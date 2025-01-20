e2dc0736cb45e94099cb2ee2bda945a66a507340
Sergey <sergey@minorodata.com>
2025-01-20 16:40:24 +0100

    Version 0.1.4

--- collet-actions/project.clj
+++ collet-actions/project.clj
@@ -1,4 +1,4 @@
-(defproject io.velio/collet-actions "0.1.4-SNAPSHOT"
+(defproject io.velio/collet-actions "0.1.4"
   :description "Actions library for Collet workflows"
   :url "https://github.com/velio-io/collet"
   :license
