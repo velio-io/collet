668934164534e653d88f98579d211c5fc3c334c2
Sergey <sergey@minorodata.com>
2025-01-20 16:46:12 +0100

    Version 0.1.5-SNAPSHOT

--- collet-app/project.clj
+++ collet-app/project.clj
@@ -1,4 +1,4 @@
-(defproject io.velio/collet-app "0.1.4"
+(defproject io.velio/collet-app "0.1.5-SNAPSHOT"
   :description "Standalone Collet application"
   :url "https://github.com/velio-io/collet"
   :license
