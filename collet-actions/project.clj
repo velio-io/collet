db45b9a7cc2ffd74fb7ab9c99f4fc9c814265fd4
Sergey <sergey@minorodata.com>
2025-01-28 23:12:28 +0100

    Version 0.2.0

--- collet-actions/project.clj
+++ collet-actions/project.clj
@@ -18,7 +18,7 @@
 
   :dependencies
   [[org.clojure/clojure "1.12.0"]
-   [io.velio/collet-core "0.1.4"]
+   [io.velio/collet-core "0.2.0"]
    [metosin/malli "0.16.4"]
    [diehard "0.11.12"]
    [http-kit "2.8.0"]
